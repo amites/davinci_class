14ed39f4262a1a31d9cd77e212855043dfc48f08
Alvin Mites <alvin@mitesdesign.com>
2016-10-03 22:47:33 -0600

    tweaking display

--- davinci_class/settings.py
+++ davinci_class/settings.py
@@ -86,8 +86,8 @@ RICHTEXT_WIDGET_CLASS = 'mezzanine_pagedown.widgets.PageDownWidget'
 RICHTEXT_FILTER_LEVEL = 3
 PAGEDOWN_SERVER_SIDE_PREVIEW = True
 
-# RICHTEXT_FILTERS = ['mezzanine_pagedown.filters.custom']
-# PAGEDOWN_MARKDOWN_EXTENSIONS = ('extra','codehilite','toc')
+RICHTEXT_FILTERS = ['mezzanine_pagedown.filters.custom']
+PAGEDOWN_MARKDOWN_EXTENSIONS = ('extra','codehilite','toc')
 
 ########################
 # MAIN DJANGO SETTINGS #
