6d4a0ed4515d100a79c38cd7b417f6d892e9e921
Alvin Mites <alvin@mitesdesign.com>
2016-10-28 22:58:43 -0600

    tweaking sync script

--- recordings/management/commands/sync_recordings_s3.py
+++ recordings/management/commands/sync_recordings_s3.py
@@ -16,34 +16,7 @@ AWS_S3_CHUNK_SIZE = 52428800  # Default to 50MB chunks
 RECORDING_FILE_EXTENSION = 'm4a'
 
 
-class Command(BaseCommand):
-    """
-    Class to scan a local directory for recording files and compare it
-    to a remote S3 bucket. It will then upload any new recordings to S3
-    and store them within a database for display. Optionally post a
-    message to slack indicating that the uploads have been made available.
-    """
-
-    help = 'Synchronizes class recordings folder with S3, adds to DB ' \
-           'for display and optionally announces new recordings to slack.'
-
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--slack',
-            action='store_true',
-            dest='slack',
-            default=False,
-            help='Announce new recordings to slack.',
-        )
-
-        parser.add_argument(
-            '--debug',
-            action='store_true',
-            dest='debug',
-            default=False,
-            help='Take a guess?'
-        )
-
+class SyncS3Command(BaseCommand):
     def __init__(self, *args, **kwargs):
         self.bucket = None
         self.conn = None
@@ -53,7 +26,7 @@ class Command(BaseCommand):
         self.s3_files = []
         self.file_extension = kwargs.get('file_extension',
                                          RECORDING_FILE_EXTENSION)
-        super(Command, self).__init__(*args, **kwargs)
+        super(SyncS3Command, self).__init__(*args, **kwargs)
 
     def check_file(self, file_name, recordings=None):
         if recordings is None:
@@ -90,6 +63,99 @@ class Command(BaseCommand):
         if get_objs:
             return self.s3_file_objs
         return self.s3_files
+
+    def sync_s3(self):
+        for bucket_file in self.get_s3_files(get_objs=True):
+            if not self.check_file(bucket_file.name):
+                continue
+            url = bucket_file.generate_url(expires_in=0, query_auth=False)
+            try:
+                ClassRecording.objects.get(url=url)
+                continue
+            except ClassRecording.DoesNotExist:
+                cr = ClassRecording(url=url)
+                cr.save()
+
+                self.stdout.write(
+                    self.style.SUCCESS('Saved {}'.format(cr.__unicode__())))
+                self.new_recordings.append(cr)
+
+    def upload_directory(self, recording_path):
+        """
+        Uploads recording files to S3 from recording_path.
+
+        :param recording_path: str: Directory path of where to find files.
+        """
+        if not os.path.isdir(recording_path):
+            self.stdout.write(self.style.ERROR('Invalid directory: {}'.format(
+                recording_path)))
+            return None
+        recordings = self.get_s3_files()
+        bucket = self.get_bucket()
+        for file_name in os.listdir(recording_path):
+            if not self.check_file(file_name, recordings):
+                continue
+            try:
+                obj = ClassRecording.objects.get(url__contains=file_name)
+                if self.debug:
+                    self.stdout.write(
+                        'Found {} already exists'.format(obj.name))
+                continue
+            except ClassRecording.DoesNotExist:
+                pass
+            self.stdout.write(self.style.NOTICE(
+                'Preparing to upload {}'.format(file_name)))
+            file_path = os.path.join(recording_path, file_name)
+            file_size = os.stat(file_path).st_size
+            mp = bucket.initiate_multipart_upload(file_name)
+            chunk_size = AWS_S3_CHUNK_SIZE
+            chunk_count = int(math.ceil(file_size / float(chunk_size)))
+
+            for i in range(chunk_count):
+                offset = chunk_size * i
+                with FileChunkIO(
+                        file_path, 'r', offset=offset,
+                        bytes=min(chunk_size, file_size - offset)) as fp:
+                    mp.upload_part_from_file(fp, part_num=i + 1)
+            mp.complete_upload()
+            bucket.make_public()
+
+    def handle(self, *args, **options):
+        """
+        The actual logic of the command. Subclasses must implement
+        this method.
+        """
+        raise NotImplementedError(
+            'subclasses of BaseCommand must provide a handle() method')
+
+
+class Command(SyncS3Command):
+    """
+    Class to scan a local directory for recording files and compare it
+    to a remote S3 bucket. It will then upload any new recordings to S3
+    and store them within a database for display. Optionally post a
+    message to slack indicating that the uploads have been made available.
+    """
+
+    help = 'Synchronizes class recordings folder with S3, adds to DB ' \
+           'for display and optionally announces new recordings to slack.'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--slack',
+            action='store_true',
+            dest='slack',
+            default=False,
+            help='Announce new recordings to slack.',
+        )
+
+        parser.add_argument(
+            '--debug',
+            action='store_true',
+            dest='debug',
+            default=False,
+            help='Take a guess?'
+        )
 
     def sync_s3(self):
         for bucket_file in self.get_s3_files(get_objs=True):
